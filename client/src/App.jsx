52d9a044eb94f6352d56222433a18b232baba531
busrakk <busrakartal160@gmail.com>
2023-04-26 10:32:14 +0300

    install react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,13 @@
-import CartTotals from "./components/cart/CartTotals";
-import Categories from "./components/categories/Categories";
-import Header from "./components/header/Header";
-import Products from "./components/products/Products";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 pb-24 md:pb-0">
-        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
-          <Categories />
-        </div>
-        <div className="products flex-[8] overflow-y-auto max-h-[calc(100vh_-_112px)] pb-10">
-          <Products />
-        </div>
-        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
-          <CartTotals />
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
