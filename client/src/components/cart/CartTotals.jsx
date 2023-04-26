04607cb113e1713847173c792520f1e4bd393736
busrakk <busrakartal160@gmail.com>
2023-04-26 04:14:18 +0300

    add  cart component

---
+++ client/src/components/cart/CartTotals.jsx
@@ -0,0 +1,77 @@
+import { Button } from "antd";
+import { ClearOutlined, PlusCircleOutlined, MinusCircleOutlined } from "@ant-design/icons";
+import React from "react";
+
+const CartTotals = () => {
+  return (
+    <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
+      <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
+        Sepetteki Ürünler
+      </h2>
+      <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://picsum.photos/200"
+              alt=""
+              className="h-16 w-16 object-cover"
+            />
+            <div className="flex flex-col ml-2">
+              <b>Ürün Adı</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center !rounded-full"
+              icon={<PlusCircleOutlined />}
+            />
+            <span>1</span>
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center !rounded-full"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+      </ul>
+      <div className="cart-totals mt-auto">
+        <div className="border-t border-b">
+          <div className="flex justify-between p-2">
+            <b>Ara Toplam</b>
+            <span>99₺</span>
+          </div>
+          <div className="flex justify-between p-2">
+            <b>KDV %8</b>
+            <span className="text-red-700">+7.92₺</span>
+          </div>
+        </div>
+        <div className="border-b mt-4">
+          <div className="flex justify-between p-2">
+            <b className="text-xl text-green-500">Genel Toplam</b>
+            <span className="text-xl">99₺</span>
+          </div>
+        </div>
+        <div className="py-4 px-4">
+          <Button type="primary" size="large" className="w-full">
+            Sipariş oluştur
+          </Button>
+          <Button
+            type="primary"
+            danger
+            size="large"
+            className="w-full mt-2 flex items-center justify-center"
+            icon={<ClearOutlined />}
+          >
+            Temizle
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartTotals;
--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Categories = () => {
   return (
     <ul className='flex gap-4 flex-col text-lg'>
-        <li className='bg-sky-500 px-6 py-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center'>
+        <li className='bg-sky-500 px-6 py-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center min-w-[145px]'>
             <span>Tümü</span>
         </li>
-        <li className='bg-sky-500 px-6 py-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center'>
+        <li className='bg-sky-500 px-6 py-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center min-w-[145px]'>
             <span>Yiyecek</span>
         </li>
-        <li className='bg-sky-500 px-6 py-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center'>
+        <li className='bg-sky-500 px-6 py-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center min-w-[145px]'>
             <span>İçecek</span>
         </li>
     </ul>
