46e04d9ca3f6c4a599dfc5e69fa680a367e1b2a9
busrakk <busrakartal160@gmail.com>
2023-05-10 16:43:53 +0300

    add client-api product listing

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-  return (
-    <div className='products-wrapper grid grid-cols-card gap-4'>
-      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
-        <div className="product-image">
-            <img src='https://picsum.photos/200' alt='' className='h-28 object-cover w-full border-b' />
-        </div>
-        <div className="product-info flex flex-col p-3">
-            <span className='font-bold'>Ürün adı</span>
-            <span>Fiyat ₺</span>
-        </div>
-      </div>
-      <div className='product-item border hover:shadow-lg cursor-pointer transition-all select-none'>
-        <div className="product-image">
-            <img src='https://picsum.photos/200' alt='' className='h-28 object-cover w-full border-b' />
-        </div>
-        <div className="product-info flex flex-col p-3">
-            <span className='font-bold'>Ürün adı</span>
-            <span>Fiyat ₺</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+  const [products, setProducts] = useState([]);
 
-export default Products
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await fetch(
+          "http://localhost:5000/api/products/get-products"
+        );
+        const data = await res.json();
+        if (data.status === "success") {
+          setProducts(data.data);
+        }
+        //console.log(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, []);
+
+  return (
+    <div className="products-wrapper grid grid-cols-card gap-4">
+      {products.map((item) => (
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+          <div className="product-image">
+            <img
+              src={item.img}
+              alt={item.name}
+              className="h-28 object-cover w-full border-b"
+            />
+          </div>
+          <div className="product-info flex flex-col p-3">
+            <span className="font-bold">{item.title}</span>
+            <span>{item.price} ₺</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
