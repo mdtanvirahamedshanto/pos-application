a592501e6fdb309b20c078c36159b94cf55c4376
busrakk <busrakartal160@gmail.com>
2023-06-18 22:12:53 +0300

    add product search

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -13,7 +13,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./index.css";
 
-const Header = () => {
+const Header = ({ setSearch }) => {
   const cart = useSelector((state) => state.cart);
   const navigate = useNavigate();
 
@@ -38,6 +38,7 @@ const Header = () => {
             placeholder="Ürün Ara..."
             prefix={<SearchOutlined />}
             className="rounded-full max-w-[800px]"
+            onChange={(e) => setSearch(e.target.value.toLowerCase())}
           />
         </div>
         <div className="menu-links">
